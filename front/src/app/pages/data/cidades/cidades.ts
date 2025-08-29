af0c530b6922b3fb30d52731c84fe6a3329d4505
Gabriel Isper <2411600431@fema.edu.br>
2025-08-29 11:11:04 -0300

    refactor(front) ♻️: mexe na funcionalidade de Cidade, utilizando o componente DynamicCrud para a realização dos CRUDs

--- front/src/app/pages/data/cidades/cidades.ts
+++ front/src/app/pages/data/cidades/cidades.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Table} from 'primeng/table';
+import Cidade from '../../../core/model/Cidade';
+import {CidadeService} from '../../../core/services/cidade-service';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {TableColumn} from '../../../core/interfaces/table-column';
+import {DynamicCrud} from '../../../core/components/dynamic-crud/dynamic-crud';
 
 @Component({
   selector: 'app-cidades',
-  imports: [],
+  imports: [
+    DynamicCrud
+  ],
+  providers: [MessageService, ConfirmationService],
   templateUrl: './cidades.html',
   standalone: true,
   styleUrl: './cidades.scss'
 })
-export class Cidades {
+export class Cidades implements OnInit {
+
+  constructor(private cidadeService: CidadeService) {
+  }
+
+  cols!: TableColumn[];
+
+  data!: Cidade[];
+
+  entityName = "Cidade";
+
+  title = "Gerenciar Cidades"
+
+  t_class = Cidade
+
+  ngOnInit(): void {
+
+    this.data = this.cidadeService.getCidades()();
+
+    this.cols = [
+      { field: 'id', header: 'ID', editable: false, type: 'number' },
+      { field: 'descricao', header: 'Descrição', editable: true, type: 'string' },
+      { field: 'estadoId', header: 'ID do Estado', editable: false, type: 'number' },
+    ];
+  }
 
 }
