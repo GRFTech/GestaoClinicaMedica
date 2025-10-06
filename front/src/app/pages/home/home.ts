f09a0b8bb1552d885097f8fca24a646d3bd107a9
DevIsper <gabrielisperb@gmail.com>
2025-10-06 02:31:51 -0300

    feat(front) ✨: adiciona a página principal do site

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import {Header} from '../../core/components/header/header';
+import {Card} from 'primeng/card';
+import {Button, ButtonDirective} from 'primeng/button';
+import {Divider} from 'primeng/divider';
+import {RouterLink} from '@angular/router';
+import {Tag} from 'primeng/tag';
+import {Panel} from 'primeng/panel';
 
 @Component({
   selector: 'app-home',
   imports: [
-    Header
+    Header,
+    Card,
+    Button,
+    Divider,
+    RouterLink,
+    Tag,
+    Panel,
+    ButtonDirective
   ],
   templateUrl: './home.html',
   standalone: true,
